0e47044b7044dc5d6150aa28a13c42b072eed023
CalvinDO <calvindelloro@mail.de>
2021-07-24 21:51:01 +0200

    audio links

--- Endabgabe/Main.ts
+++ Endabgabe/Main.ts
@@ -43,9 +43,9 @@ namespace Endabgabe {
         public static rotator: ƒ.Node;
 
 
-        public static rotateSound: ƒ.Audio = new ƒ.Audio("/PRIMA/Audio/rotate.wav");
-        public static backgroundSound: ƒ.Audio = new ƒ.Audio("/PRIMA/Audio/background.wav");
-        public static winSound: ƒ.Audio = new ƒ.Audio("/PRIMA/Audio/win.mp3");
+        public static rotateSound: ƒ.Audio = new ƒ.Audio("/Audio/rotate.wav");
+        public static backgroundSound: ƒ.Audio = new ƒ.Audio("/Audio/background.wav");
+        public static winSound: ƒ.Audio = new ƒ.Audio("/Audio/win.mp3");
 
         public static cmpRotateSound: ƒ.ComponentAudio = new ƒ.ComponentAudio(Main.rotateSound);
         public static cmpBackgroundSound: ƒ.ComponentAudio = new ƒ.ComponentAudio(Main.backgroundSound);
